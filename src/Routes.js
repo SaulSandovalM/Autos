e1bf5d4db58f9fd7739c6564a7be2d9f8b1827a2
SaulSandovalM <sauldevelop@gmail.com>
2020-03-31 11:31:29 -0600

    home responsive y citas

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Home from './components/home/Home';
-import TablePachuca from "./components/tables/pachuca/TablePachuca";
+import TablaPachuca from "./components/tables/pachuca/TablaPachuca";
+import Filter from "./components/tables/pachuca/Filter";
 import LoginContainer from "./components/login/LoginContainer";
 
 export const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home}/>
-    <Route exact path="/TablePachuca" component={TablePachuca} />
+    <Route exact path="/CitasPachuca" component={TablaPachuca} />
+    <Route exact path="/Filtro" component={Filter} />
     <Route exact path="/Login" component={LoginContainer} />
   </Switch>
 );
---
+++ src/components/tables/pachuca/RowComponent.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import '../Tables.css';
+
+class RowComponent extends Component {
+  constructor(props){
+     super(props);
+     this.state = {
+       done: false,
+       item: 'Atendido',
+     };
+   }
+
+   update = () => {
+     this.props.update(this.props.item);
+   }
+
+  render() {
+
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+      dd = '0' + dd
+    }
+    if(mm < 10){
+      mm = '0' + mm
+    }
+
+    today = yyyy + '-' + mm + '-' + dd;
+
+    var d = new Date();
+    var n = d.getHours();
+
+    const fechaC = this.props.item.fecha;
+    const horaC = this.props.item.hora;
+    const sedeC = this.props.item.municipio;
+    var trans = parseInt(horaC);
+    let table;
+
+    if (fechaC === today && trans >= n) {
+      table =
+        <div className="products-al">
+          <div className="data-table">{this.props.item.nombre} {this.props.item.apellidop} {this.props.item.apellidom}</div>
+          <div className="data-table">{this.props.item.placas}</div>
+          <div className="data-table">{this.props.item.modelo}</div>
+          <div className="data-table">{this.props.item.color}</div>
+          <div className="data-table">{this.props.item.fecha} / {this.props.item.hora}</div>
+          <div className="data-table">
+            {this.props.item.status}
+            <button onClick={this.update}>Atendido</button>
+          </div>
+        </div>;
+    }
+    return (
+      <div>
+       {table}
+      </div>
+    );
+  }
+}
+
+export default RowComponent;
